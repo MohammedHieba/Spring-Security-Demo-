4d1b47d3a28b41b38d5c4475fd1ee3ebeec44dd7
Mohamed Hieba <76825840+MohammedHieba@users.noreply.github.com>
2025-08-15 18:04:26 +0300

    add refresh token && logout endpoints

--- src/main/java/com/spring/security/demo/repository/TokenInfoRepository.java
+++ src/main/java/com/spring/security/demo/repository/TokenInfoRepository.java
@@ -5,6 +5,10 @@ import com.spring.security.demo.model.TokenInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TokenInfoRepository extends JpaRepository<TokenInfo, Long> {
+    Optional<TokenInfo> findByAccessToken(String accessToken);
+    Optional<TokenInfo> findByRefreshToken(String refreshToken);
 }
